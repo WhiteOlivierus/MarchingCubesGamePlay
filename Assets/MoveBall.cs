0ae5ead4e83e9593e89bc6aff49ce96f56319531
DutchSkull <bastien@dutchskull.com>
2020-04-13 19:55:14 +0200

    Made generated ball follow mouse position

---
+++ Assets/MoveBall.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class MoveBall : MonoBehaviour
+{
+    public MeshGenerator meshGenerator;
+
+    private void Update()
+    {
+        meshGenerator.offset = GetMouseWorldspace();
+        meshGenerator.RequestMeshUpdate();
+    }
+
+    private Vector3 GetMouseWorldspace()
+    {
+        var v3 = Input.mousePosition;
+        v3.z = 10f;
+        v3 = Camera.main.ScreenToWorldPoint(v3);
+        v3.z = 0f;
+        return v3 * -1;
+    }
+}
