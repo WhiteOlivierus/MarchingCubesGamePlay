bd42acc0af8bc626673ed5308604eae187ad5559
Bastien Olivier Dijkstra <bas10olivier@hotmail.com>
2020-04-20 19:39:15 +0200

    Added a base for chunk to get the mouse in side of the chunk in all times.

---
+++ Assets/MousePositionInMesh.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+public class MousePositionInMesh : MonoBehaviour
+{
+    public float boundsSize = 1;
+    private Vector3 cursorPosition;
+    private Vector3 positiveCorner;
+    private Vector3 negativeCorner;
+    private CharacterController con;
+
+    private void Awake() => con = transform.parent.GetComponent<CharacterController>();
+
+    private void Update()
+    {
+        UpdateBounds();
+
+        cursorPosition = GetMouseWorldspace(boundsSize);
+
+        positiveCorner = CreatePositiveCorner();
+        negativeCorner = CreateNegativeCorner();
+
+        cursorPosition = cursorPosition.Clamp(negativeCorner, positiveCorner);
+    }
+
+    private Vector3 CreatePositiveCorner() => new Vector3(transform.position.x + (transform.localScale.x / 2),
+                                               transform.position.y + (transform.localScale.y / 2),
+                                               transform.position.z + (transform.localScale.z / 2));
+
+    private Vector3 CreateNegativeCorner() => new Vector3(transform.position.x - (transform.localScale.x / 2),
+                                               transform.position.y - (transform.localScale.y / 2),
+                                               transform.position.z - (transform.localScale.z / 2));
+
+    private Vector3 GetMouseWorldspace(float boundsSize)
+    {
+        Vector3 v3 = Input.mousePosition;
+        v3.z = boundsSize;
+        v3 = Camera.main.ScreenToWorldPoint(v3);
+        v3.z = boundsSize;
+        return v3;
+    }
+
+    private void UpdateBounds()
+    {
+        transform.localScale = Vector3.one * boundsSize;
+        transform.localPosition = new Vector3(0, (boundsSize / 2) - (con.height / 2), (boundsSize / 2) + con.radius);
+    }
+
+    private void OnDrawGizmos()
+    {
+        con = transform.parent.GetComponent<CharacterController>();
+
+        UpdateBounds();
+
+        positiveCorner = CreatePositiveCorner();
+        negativeCorner = CreateNegativeCorner();
+
+        Gizmos.color = new Color(1f, 0, 0, 0.5f);
+        Gizmos.DrawCube(transform.position, Vector3.one * boundsSize);
+
+        Gizmos.color = new Color(0, 0, 1f, 0.5f);
+        Gizmos.DrawSphere(cursorPosition, .25f);
+
+        Gizmos.color = new Color(0, 1f, 0, 0.5f);
+        Gizmos.DrawSphere(positiveCorner, .10f);
+
+        Gizmos.color = new Color(0, 1f, 0, 0.5f);
+        Gizmos.DrawSphere(negativeCorner, .10f);
+    }
+}
---
+++ Assets/Vector3Extension.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public static class Vector3Extension
+{
+    public static Vector3 Clamp(this Vector3 vector, Vector3 min, Vector3 max)
+    {
+        vector.x = Mathf.Clamp(vector.x, min.x, max.x);
+        vector.y = Mathf.Clamp(vector.y, min.y, max.y);
+        vector.z = Mathf.Clamp(vector.z, min.z, max.z);
+        return vector;
+    }
+}
